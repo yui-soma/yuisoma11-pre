466f746226e8fec5d4711c3d921f82fe8c4fcbf2
相馬由衣 <somayui@soumayuinoMacBook-Pro.local>
2021-01-08 07:51:09 +0900

    ハンバーガーメニューをクリックしたらアクティブになるようにJS追加

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("ham").addEventListener("click", function() {
+        this .classList .toggle("active");
+    })
+} );
