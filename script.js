564da4aee46cfd61f38ace97a4f592a76e9cd724
相馬由衣 <somayui@soumayuinoMacBook-Pro.local>
2021-01-08 17:42:22 +0900

    ハンバーガーメニュー実装

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("ham").addEventListener("click", function() {
         this .classList .toggle("active");
+        document.getElementById("nav").classList.toggle("active");
+        document.getElementById("mask").classList.toggle("active");
     })
 } );
